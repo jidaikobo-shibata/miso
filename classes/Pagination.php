6a48203a9b3d44ac4b41d29569a7d02f49695ffe
HajimeKonagai <konagai@jidaikobo.com>
2018-09-07 19:11:23 +0900

    add

---
+++ classes/Pagination.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Miso\Pagination
+ *
+ * @package    part of Miso
+ * @author     Jidaikobo Inc.
+ * @license    The MIT License (MIT)
+ * @copyright  Jidaikobo Inc.
+ * @link       http://www.jidaikobo.com
+ */
+namespace Miso;
+
+class Pagination
+{
+}
